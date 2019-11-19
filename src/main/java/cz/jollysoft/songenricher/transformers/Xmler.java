ab0ceba772a9248d01f0adb7964d43e88dcd02c8
Pavel Foltyn <pfoltyn@newps.cz>
2019-11-19 17:43:24 +0100

    FIX SongEnricher - Trim the contents of a token and skip empty tokens.

--- src/main/java/cz/jollysoft/songenricher/transformers/Xmler.java
+++ src/main/java/cz/jollysoft/songenricher/transformers/Xmler.java
@@ -109,7 +109,7 @@ public class Xmler {
 
         // Prepare a resulting list of tokens.
         List<Token> tokens = new ArrayList<>();
-        Token token;
+        //Token token;
 
         // Scan characters within the string, one by one.
         int currentPos = 0;
@@ -128,7 +128,9 @@ public class Xmler {
                 // Markup token.
                 // Search for the first '>'. This shall be the end of this markup token (inclusive).
                 // Skip any sequences quoted with single quotes or double quotes.
-                int pos = currentPos;
+                //int pos = currentPos;
+                // Skip the character that the current token begins with.
+                int pos = currentPos + 1;
                 boolean endOfMarkupFound = false;
                 boolean insideQuotedSequence = false;
                 char quotingMark = '"';
@@ -136,8 +138,9 @@ public class Xmler {
                     char cc = text.charAt(pos);
                     if ( (cc == '>') && ( ! insideQuotedSequence ) ) {
                         // End of this markup token has been encountered.
-                        token = new Token(text.substring(currentPos, pos + 1));
-                        tokens.add(token);
+                        //token = new Token(text.substring(currentPos, pos + 1));
+                        //tokens.add(token);
+                        addToken(tokens, text, currentPos, pos + 1);
                         currentPos = pos + 1;
                         endOfMarkupFound = true;
                         break;
@@ -170,7 +173,10 @@ public class Xmler {
 
                 // Text token.
                 // Search for the first '<'. This shall be the end of this text token (exclusive).
-                int lessThanSignPos = text.indexOf('<', currentPos);
+                //int lessThanSignPos = text.indexOf('<', currentPos);
+                // Skip the character that the current token begins with.
+                // The indexOf(int ch, int fromIndex) method works fine even for situations where the fromIndex parameter points outside the string.
+                int lessThanSignPos = text.indexOf('<', currentPos + 1);
 
                 // Handle both cases (found / not found).
                 if (lessThanSignPos >= 0) {
@@ -179,13 +185,15 @@ public class Xmler {
                         throw new RuntimeException(String.format("The current position in the tokenizer is %d. But the '<' has been found at %d.", currentPos, lessThanSignPos));
                     }
                     // Start of another token found.
-                    token = new Token(text.substring(currentPos, lessThanSignPos));
-                    tokens.add(token);
+                    //token = new Token(text.substring(currentPos, lessThanSignPos));
+                    //tokens.add(token);
+                    addToken(tokens, text, currentPos, lessThanSignPos);
                     currentPos = lessThanSignPos;
                 } else {
                     // No more tokens.
-                    token = new Token(text.substring(currentPos, endPos));
-                    tokens.add(token);
+                    //token = new Token(text.substring(currentPos, endPos));
+                    //tokens.add(token);
+                    addToken(tokens, text, currentPos, endPos);
                     currentPos = endPos;
                 }
 
@@ -195,6 +203,49 @@ public class Xmler {
 
         // Return the result.
         return tokens;
+
+    }
+
+
+
+    /**
+     * Extracts a token from a given text and adds it to a given list of tokens.
+     * The method leaves out leading and trailing whitespace characters. This is called "trimming".
+     * If the result of the trim operation (from both the beginning and the end) is an empty string,
+     * then the "empty" token is ignored and thus not added to the resulting token list.
+     * 
+     * @param tokens List of tokens to add the new token to.
+     * @param text Text to extract the token out of.
+     * @param startPos Starting position of the token (inclusive).
+     * @param endPos Ending position of the token (exclusive).
+     */
+    private void addToken(List<Token> tokens, String text, int startPos, int endPos) {
+
+        // Integrity check:
+        // Starting position must fall within the given text string.
+        // Ending position must fall within the given text string, or it can be equal to the text's length (but must not go beyond it).
+        // Starting position must be less than the ending position (the token must have one character at least).
+        if ( ! ((startPos >= 0) && (startPos < text.length())) ) {
+            throw new RuntimeException(String.format("The starting position of %d is outside the valid indices for the given string <%d, %d).", startPos, 0, text.length()));
+        }
+        if ( ! ((endPos >= 0) && (endPos <= text.length())) ) {
+            throw new RuntimeException(String.format("The ending position of %d is outside the valid indices for the given string <%d, %d>.", startPos, 0, text.length()));
+        }
+        if ( ! (startPos < endPos) ) {
+            throw new RuntimeException(String.format("The starting position is %d, the ending position is %d. Valid positions for the given string are <%d, %d). The starting position should be LESS THAN the ending position.", startPos, endPos, 0, text.length()));
+        }
+
+        // Extract the token.
+        String tokenAsString = text.substring(startPos, endPos);
+
+        // Trim the token.
+        String tokenAsStringTrimmed = tokenAsString.trim();
+
+        // If there is something left after trimming, create a Token object out of the remainder and add it to the list.
+        if (tokenAsStringTrimmed.length() > 0) {
+            Token token = new Token(tokenAsStringTrimmed);
+            tokens.add(token);
+        }
 
     }
 
